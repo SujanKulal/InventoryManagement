2f284b3bcd9dd1b06668e307ed1fabbad68df62e
sujan <sujankulal19@gmail.com>
2025-10-02 21:51:50 +0530

    commit

---
+++ src/test/java/com/inventory_management/service/ProductServiceTest.java
@@ -0,0 +1,45 @@
+package com.inventory_management.service;
+
+import com.inventory_management.entity.Product;
+import com.inventory_management.repository.ProductRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class ProductServiceTest {
+
+    @Mock
+    private ProductRepository productRepository;
+
+    @InjectMocks
+    private ProductService productService;
+
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.openMocks(this);;
+    }
+
+    @Test
+    void testIncreaseStock(){
+        Product product = new Product();
+        product.setId(1L);
+        product.setName("Laptop");
+        product.setDescription("Test Laptop");
+        product.setStockQuantity(10L);
+
+        when(productRepository.findById(1L)).thenReturn(Optional.of(product));
+        when(productRepository.save(any(Product.class))).thenAnswer(i->i.getArguments()[0]);
+
+        Product updated = productService.increaseStock(1L,5);
+
+        assertEquals(15, updated.getStockQuantity());
+    }
+
+}
